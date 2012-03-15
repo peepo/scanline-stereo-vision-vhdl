380fdc821fb730d643a20cd7deaa182c62db3c0f
x <x@x0.alfa.com>
2012-03-16 00:36:38 +0100

    LR pipiline draft

--- scanlineRD_LR_UD/const.vhd
+++ scanlineRD_LR_UD/const.vhd
@@ -6,14 +6,13 @@ use IEEE.std_logic_unsigned.all;
 package const is
 
 
-constant dmax	  	: integer := 64;
+constant dmax	  	: integer := 16;
 constant P1			: integer := 10; 
 constant P2			: integer := 40;  
-constant scale 	: integer := 10;
---constant Width		: integer := 384;
---constant Hight		: integer := 288;
-constant Width		: integer := 640;
-constant Hight		: integer := 480;
+constant Width		: integer := 384;
+constant Hight		: integer := 288;
+--constant Width		: integer := 640;
+--constant Hight		: integer := 480;
 
 constant ADDR_WIDTH	: integer := 10;  
 constant DATA_WIDTH	: integer := 8;
@@ -32,12 +31,12 @@ type	RL_Line_array is array(1 downto 0, Width - 1 downto 0) of pixel;
 type	line_disparity is array(Width - 1 downto 0) of std_logic_vector(7 downto 0);
 type	line_global_cost_array is array(Width - 1 downto 0) of GlobalCosts_array;
 type	Address_Width_array is array (dmax - 1 downto 0) of std_logic_vector (ADDR_WIDTH - 1 downto 0);
-type  d_disp_array is array(3 downto 0) of int_64;
+type	d_disp_array is array(3 downto 0) of int_64;
 
-type  d_pl0_array is array(dmax/4 - 1 downto 0) of int_64;
-type  d_pl1_array is array(dmax/16 - 1 downto 0) of int_64;
-type  GC0_array is array(dmax/4 - 1 downto 0) of LP_Element;
-type  GC1_array is array(dmax/16 - 1 downto 0) of LP_Element;
+type	d_pl0_array is array(dmax/4 - 1 downto 0) of int_64;
+type	d_pl1_array is array(dmax/16 - 1 downto 0) of int_64;
+type	GC0_array is array(dmax/4 - 1 downto 0) of LP_Element;
+type	GC1_array is array(dmax/16 - 1 downto 0) of LP_Element;
 
 type    GC_array is array((dmax - 1)/3 downto 0) of LP_Element;
 
