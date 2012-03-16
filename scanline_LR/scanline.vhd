6daef18ee90f485774a92763e27937e6dbf719be
x <x@x0.alfa.com>
2012-03-16 01:31:37 +0100

    LR new pipeline final

--- scanline_LR/scanline.vhd
+++ scanline_LR/scanline.vhd
@@ -46,10 +46,6 @@ architecture Behavioral of scanline is
 	signal dpl1			: d_pl1_array;
 	signal GC0			: GC0_array;
 	signal GC1			: GC1_array;
-	signal dpl0v			: d_pl0_array;
-	signal dpl1v			: d_pl1_array;
-	signal GC0v			: GC0_array;
-	signal GC1v			: GC1_array;
 begin
 
 
@@ -214,7 +210,7 @@ begin
 			if RESET = '1' then
 				dpl0(k)	<= 0;
 				GC0(k)	<= (others => '0');
-			elsif LINE_VALID_IN = '1'  then	
+			elsif LINE_VALID_IN = '1' then	
 				d := k * 4;
 				for z in k * 4  to (k + 1) * 4 - 1 loop
 					if(comparator1(GLOBAL_COST(d), GLOBAL_COST(z))) then
@@ -222,7 +218,7 @@ begin
 					end if;
 				end loop;				
 				dpl0(k)	<= d;
-				GC0(k)		<= GLOBAL_COST(d);
+				GC0(k)	<= GLOBAL_COST(d);
 			end if;
 		end if;	
 	end process DPL00;
@@ -237,7 +233,7 @@ begin
 			if RESET = '1' then
 				dpl1(k)	<= 0;
 				GC1(k)	<= (others => '0');
-			elsif LINE_VALID_IN = '1'  then					
+			elsif LINE_VALID_IN = '1' then					
 				d := k * 4;
 				for z in k * 4 to (k + 1) * 4 - 1 loop
 					if(comparator1(GC0(d), GC0(z))) then
@@ -254,151 +250,37 @@ end generate PL1;
 
 DISP : process(PIPELINE_CLOCK) is	
 	variable d		: int_64;
-	variable d0		: int_64;
-	variable d1		: int_64;
-	variable d2		: int_64;
-	variable d3		: int_64;
 	variable dLR	: int_64;
 	variable GlobalCostLR : GlobalCosts_array;
 	
---	variable dpl0v			: d_pl0_array;
---	variable dpl1v			: d_pl1_array;
---	variable GC0v			: GC0_array;
---	variable GC1v			: GC1_array;
 begin
 	if PIPELINE_CLOCK = '1' and PIPELINE_CLOCK'EVENT  then
 		if RESET = '1' then
 			GLOBAL_COST_PREV <= (others =>(others => '0'));
-			d := 0;
 			dLR := 0;
 			dd <= (others => '0');
-			
-			GC0v <= (others =>(others => '0'));
-			GC1v <= (others =>(others => '0'));
-			dpl0v <= (others => 0);
-			dpl1v <= (others => 0);
 		elsif LINE_VALID_IN = '1'  then	
+			d := 0;
+			for z in 0 to dmax / 32 -1 loop
+				if(comparator1(GC1(d), GC1(z))) then
+					d := z;
+				end if;				
+			end loop;
+			d := dpl1(d);
 			
---			d1 := 0;
---			d2 := 0;
---			d3 := 0;
-			
---			d := 0;
---			for k in 0 to dmax - 1 loop
---				if(comparator1(GLOBAL_COST(d), GLOBAL_COST(k))) then
---					d := k;
---				end if;
---			end loop;
-
---			for k in 0 to dmax / 4 - 1 loop
---				d := k * 4;
---				for z in k * 4  to (k + 1) * 4 - 1 loop
---					if(comparator1(GLOBAL_COST(d), GLOBAL_COST(z))) then
---						d := z;
---					end if;
---				end loop;				
---				dpl0v(k)	<= d;
---				GC0v(k)		<= GLOBAL_COST(d);
---			end loop;
---			
---			for k in 0 to dmax / 16 - 1 loop
---				d := k * 4;
---				for z in k * 4 to (k + 1) * 4 - 1 loop
---					if(comparator1(GC0v(d), GC0v(z))) then
---						d := z;
---					end if;
---				end loop;				
---				dpl1v(k)	<= dpl0v(d);
---				GC1v(k)		<= GC0v(d);			
---			end loop;
-
-
-
---			d := 0;
---			for k in 0 to dmax - 1 loop
---				if(comparator1(GLOBAL_COST(d), GLOBAL_COST(k))) then
---					d := k;
---				end if;
---			end loop;
-
---			d := dpl1v(0);
-			
-
-
---			
---			d0 := 0 * 4;
---			for k in 0 to dmax / 4 - 1 loop
---				if(comparator1(GLOBAL_COST(d0), GLOBAL_COST(k))) then
---					d0 := k;
---				end if;
---			end loop;
---			
---			d1 := 1 * 4;
---			for k in dmax / 4 to 2*dmax / 4 - 1 loop
---				if(comparator1(GLOBAL_COST(d1), GLOBAL_COST(k))) then
---					d1 := k;
---				end if;
---			end loop;
---
---			d2 := 2 * 4;
---			for k in 2*dmax / 4 to 3*dmax / 4 - 1 loop
---				if(comparator1(GLOBAL_COST(d2), GLOBAL_COST(k))) then
---					d2 := k;
---				end if;
---			end loop;
---
---			d3 := 3 * 4;
---			for k in 3*dmax / 4 to 4*dmax / 4 - 1 loop
---				if(comparator1(GLOBAL_COST(d3), GLOBAL_COST(k))) then
---					d3 := k;
---				end if;
---			end loop;
---
---
---			d:= d3;
---			if(comparator1(GLOBAL_COST(d3), GLOBAL_COST(d2))) then
---				d := d2;
---			end if;
---			if(comparator1(GLOBAL_COST(d), GLOBAL_COST(d1))) then
---				d := d1;
---			end if;
---			if(comparator1(GLOBAL_COST(d), GLOBAL_COST(d1))) then
---				d := d1;
---			end if;
-
 			if i = "00" then
 				GLOBAL_COST_PREV <= (others =>(others => '1'));			
 			elsif i = "01" then
-				
 				GlobalCostLR := GLOBAL_COST;
-				
---				dLR := d;
 				GLOBAL_COST_PREV <= (others =>(others => '1'));
 			elsif i = "10" then
 				GLOBAL_COST_PREV <= (others =>(others => '0'));
 				dd <= conv_std_logic_vector(8, 6);
 			elsif i= "11" then
 				GLOBAL_COST_PREV <= GlobalCostLR;
---				dd <= conv_std_logic_vector(dLR, 6);
---				dLR := dpl1v(0);
-				dLR := dpl1(0);
-				DATA_OUT <= conv_std_logic_vector(dLR , 4) & "0000";				
+				dLR := d;
+				DATA_OUT <= conv_std_logic_vector(dLR , 5) & "000";				
 				dd <= conv_std_logic_vector(dLR, 6);
---				d := 0;
---				for z in 0 to dmax / 16 - 1 loop
---					if(comparator1(GC1(d), GC1(z))) then
---						d := z;
---					end if;
---				end loop;
---				d := dpl1(d);
---				DATA_OUT <= conv_std_logic_vector(d , 5) & "000";				
---				dd <= conv_std_logic_vector(d, 6);
-
---				if dpl1(0) = dpl1v(0) then
---					report "1111111111111111";
---				else
---					report "0000000000000000";
---				end if;
 			end if;
 		end if;
 	end if;
